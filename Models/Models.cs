80d6c9190162abae75cdebc455a3925f25238e38
Ezequiel Demetras <ezequiel-d.s@hotmail.com>
2026-05-07 22:27:58 -0300

    fix mcc type

--- Models/Models.cs
+++ Models/Models.cs
@@ -8,7 +8,7 @@ public record Transaction(float amount, int installments, DateTime requested_at)
 
 public record Customer(float avg_amount, int tx_count_24h, string[] known_merchants);
 
-public record Merchant(string id, int mcc, float avg_amount);
+public record Merchant(string id, string mcc, float avg_amount);
 
 public record Terminal(bool is_online, bool card_present, float km_from_home);
 
